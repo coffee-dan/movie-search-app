71894d20ddeb234e4bc8fb56e5800788ca8b59d1
coffee-dan <coffee-dan@protonmail.com>
2020-10-08 12:41:33 -0500

    Styling added and refactored SearchMovies
    Comments provided for unfamiliar CSS techniques
    create-react-app default font styling reintroduced

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -8,15 +8,13 @@ export default function SearchMovies() {
     
     return (
         <form className="form">
-            <label className="label" htmlFor="query">
-                Movie Name
-                <input 
-                    className="input"
-                    type="text"
-                    name="query"
-                    placeholder="i.e. Jurassic Park"
-                />
-            </label>
+            <label className="label" htmlFor="query">Movie Name</label>
+            <input 
+                className="input"
+                type="text"
+                name="query"
+                placeholder="i.e. Jurassic Park"
+            />
             <button className="button" type="submit">Submit</button>
         </form>
     )
